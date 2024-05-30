5610b91b3676115d6838283e66209d1a6311ad80
Liban <libanhassannoor@gmail.com>
2024-05-30 08:18:51 +0300

    displaying text to the screen

---
+++ printf.c
@@ -0,0 +1,12 @@
+// importing the standard/input output library
+#include <stdio.h>
+//this is the main c function where code execute begins
+int main()
+{
+    //Displaying text to the screen
+    printf("My name is liban\n");
+    printf("basketball is my favorite sport\n");
+    printf("i am tall\n");
+    printf("i love coding\n");
+
+} 
