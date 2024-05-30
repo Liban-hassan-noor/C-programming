df6cc3053b7f5301a30ae4cc1937e3c86e6685e0
Liban <libanhassannoor@gmail.com>
2024-05-30 08:41:01 +0300

    taking input from  user

---
+++ user_input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int add-number (int num1, int num2)
+{
+    return num1 + num2;
+}
+int main()
+{
+    int number1,number2,result;
+    printf("Enter the First Number");
+    scanf("%d, &number1");
+
+    printf("Enter the Seconf Number");
+    scanf("%d, &number2");
+
+    printf("The Sum Of %dand %d is %dn, number1,number2,result");
+
+    return0;
+}
